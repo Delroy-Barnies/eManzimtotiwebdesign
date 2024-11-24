886e8dc2a342393e8a6eef66d8e92b97ada218fb
DelroyBarnies <Delroybarnies3@gmail.com>
2024-11-24 17:01:23 +0200

    reactdom

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import routes from "./routes";
 
 const router = createBrowserRouter(routes);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </React.StrictMode>,
 )
+
