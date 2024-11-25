62c2cd8a0cbb1ef67bc6490390e2110493c2c2be
DelroyBarnies <Delroybarnies3@gmail.com>
2024-11-25 13:23:49 +0200

    sidebar buttons added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,9 @@ import { HashLink } from 'react-router-hash-link';
 import burgerMenu from '../../assets/burger_menu.svg';
 import companyLogoIcon from '../../assets/company_logo.svg';
 import facebookIcon from '../../assets/facebook.svg';
+import facebookSidebarIcon from '../../assets/facebook_sidebar.svg';
 import whatsappIcon from '../../assets/whatsapp.svg';
+import whatsappSidebarIcon from '../../assets/whatsapp_sidebar.svg';
 import styles from "./Header.module.css";
 
 export default function Header() {
@@ -45,7 +47,11 @@ export default function Header() {
             </div>
 
             <div className={styles.sidebar} style={{ transform: `translateX(${sidebar})` }}>
-                <button className={styles.burger_menu} onClick={setSidebarStatus}><img className={styles.menu} alt="company logo icon" src={burgerMenu} /></button>
+                <div>
+                    <button className={styles.burger_menu} onClick={setSidebarStatus}><img className={styles.menu} alt="company logo icon" src={burgerMenu} /></button>
+                    <a className={styles.burger_menu} href="https://www.facebook.com/profile.php?id=61567671659062" ><img className={styles.fb_sidebar} alt="company logo icon" src={facebookSidebarIcon} /></a>
+                    <a className={styles.burger_menu} href="https://l.facebook.com/l.php?u=https%3A%2F%2Fapi.whatsapp.com%2Fsend%3Fphone%3D%252B27761919226%26context%3DARBw3nt_BWewOEenF69_gjcuG-dId3U8B1lgRSMM9CVIMu65vjzGt9H5UbW26akrGpC_cpbTrW6tjtHyC-apD6RRcf_9oyQrTVxs6GSgeq_co_dCeCrsIE8ePotECGY_vvi2Rgi5WlfuqkNf9q-EvWVByw%26source%3DFB_Page%26app%3Dfacebook%26entry_point%3Dpage_cta%26fbclid%3DIwZXh0bgNhZW0CMTAAAR0iA-CXHd_cpmvD5RigaNreCAsq0Ue1iSNWANz5ls-Kkv2ZQfjtoguqswc_aem_mU1_TxzHm6YIS1gcyrPmzQ&h=AT2hOSJyQzoyoxCS-wgjLF0PY9pAsqiVrVjRIr9y9vUENcmcm__4Hl7bvbkVIUy4p_4oCOv-q0LBWqxD72b26rBr6EXxasJqFHAcvVpL6oEEhwTAzaXeV2D0VWDaenPF-tvJbw" ><img className={styles.wa_sidebar} alt="company logo icon" src={whatsappSidebarIcon} /></a>
+                </div>
                 <nav className={styles.sidebar_links_nav}>
                     <Link onClick={setSidebarStatus} className={styles.sidebar_link} to="../" >Home</Link>
                     <HashLink onClick={setSidebarStatus} smooth to="/#about" className={styles.sidebar_link} >About</HashLink>
